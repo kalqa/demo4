d3eeb030a22ecf57bc2c8b0906f0d387da991cab
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-05-15 12:02:36 +0200

    reset

--- src/main/java/com/example/demo/model/Journal.java
+++ src/main/java/com/example/demo/model/Journal.java
@@ -1,14 +1,5 @@
 package com.example.demo.model;
 
-import org.springframework.stereotype.Component;
-
-//@Component
 public class Journal {
-
-    private String name;
-
-    public Journal(String name) {
-        this.name = name;
-    }
 
 }
--- src/main/java/com/example/demo/model/Umbrella.java
+++ src/main/java/com/example/demo/model/Umbrella.java
@@ -1,17 +1,4 @@
 package com.example.demo.model;
 
-import javax.annotation.PostConstruct;
-
-//@Component
 public class Umbrella {
-
-    private String name;
-
-    public Umbrella(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
